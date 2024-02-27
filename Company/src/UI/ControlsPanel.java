69d8db76c345fe9c566d859ce8f08ce07a46894a
pavel <pavel.smirnov121@gmail.com>
2024-02-27 13:34:50 +0100

    fuck it, we ball - inteliJ UI Designer

---
+++ Company/src/UI/ControlsPanel.java
@@ -0,0 +1,17 @@
+package UI;
+
+import javax.swing.*;
+
+public class ControlsPanel {
+    private JTextField seatradeIP;
+    private JTextField seatradePort;
+    private JTextField companyPort;
+    private JButton startServerButton;
+    private JButton restartServerButton;
+    private JButton openWebSiteButton;
+    private JPanel mainPanel;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
