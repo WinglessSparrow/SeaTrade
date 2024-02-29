92998851507915474d6ecbc7725fb0ac23c12691
Pavel <39067541+WinglessSparrow@users.noreply.github.com>
2024-02-29 16:10:50 +0100

    interactive map is actually interactive

--- Company/resources/script.js
+++ Company/resources/script.js
@@ -16,9 +16,94 @@ const harbourGrid = new gridjs.Grid({
 
 async function getData() {
     try {
-        const resp = await fetch("http://localhost:9000/data");
-        const data = await resp.json();
-        console.log(data);
+        // const resp = await fetch("http://localhost:9000/data");
+        // const data = await resp.json();
+        // console.log(data);
+
+        const data = {
+                "company": {
+                    "name": "East India Trading Company",
+                    "deposit": 50000000,
+                    "mapSize": {
+                        "width": 30,
+                        "height": 20
+                    },
+                    "id": 30
+                },
+                "cargosData": [
+                    ["90", "145", "150"],
+                    ["91", "146", "152"],
+                    ["92", "150", "145"],
+                ],
+                "shipsData": [
+                    ["Titanic", "17", "0", "UP", "90", "145"],
+                    ["Uss Gerald R. Ford", "7", "10", "UP", "91", "Null"],
+                    ["JP Kaga", "5", "2", "UP", "Null", "Null"]
+                ],
+                "harboursData": [
+                    [
+                        "145",
+                        "reykjavik",
+                        "17",
+                        "0"
+                    ],
+                    [
+                        "146",
+                        "lissabon",
+                        "25",
+                        "9"
+                    ],
+                    [
+                        "147",
+                        "dakar",
+                        "24",
+                        "16"
+                    ],
+                    [
+                        "148",
+                        "algier",
+                        "29",
+                        "13"
+                    ],
+                    [
+                        "149",
+                        "cotonau",
+                        "29",
+                        "18"
+                    ],
+                    [
+                        "150",
+                        "halifax",
+                        "2",
+                        "3"
+                    ],
+                    [
+                        "151",
+                        "plymouth",
+                        "29",
+                        "0"
+                    ],
+                    [
+                        "152",
+                        "brest",
+                        "28",
+                        "5"
+                    ],
+                    [
+                        "153",
+                        "new york",
+                        "0",
+                        "10"
+                    ],
+                    [
+                        "154",
+                        "carracas",
+                        "2",
+                        "18"
+                    ]
+                ]
+            }
+        
 
         if (JSON.stringify(currData) != JSON.stringify(data)) {
             currData = data;
@@ -58,9 +143,9 @@ async function getData() {
                 .forceRender();
 
             document.querySelector("#log").textContent = "";
+            
+            render();
         }
-
-        render();
     } catch (e) {
         console.log(e);
         document.querySelector("#log").innerHTML = "<i class='ti ti-bug'></i> Couldn't fetch new Data from the Backend";
@@ -70,43 +155,98 @@ async function getData() {
 const render = () => {
     const map = document.querySelector("#interactive-map > #map");
 
-    let harbsPairs = [];
-    let shipPairs = [];
-
-    currData.harboursData.forEach((val) => {
-        harbsPairs.push([val[2], val[3]]);
-    });
-
-    currData.shipsData.forEach((val) => {
-        shipPairs.push([val[2], val[3]]);
-    });
-
-    console.log(harbsPairs);
-
     if (currData != null) {
-        for (let i = 0; i < currData.company.mapSize.height; i++) {
-            for (let j = 0; j < currData.company.mapSize.width; j++) {
+        for (let x = 0; x < currData.company.mapSize.width; x++) {
+            for (let y = 0; y < currData.company.mapSize.height; y++) {
                 const node = document.createElement("div");
-                node.classList = ["cell"];
-                node.style = `--x:${j + 1}; --y:${i + 1};`;
 
-                if (shipPairs.some((a) => a[0] === j + "" && a[1] === i + "")) {
-                    node.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ship" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M2 20a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1" /><path d="M4 18l-1 -5h18l-2 4" /><path d="M5 13v-6h8l4 6" /><path d="M7 7v-4h-1" /></svg>';
+                node.classList.add("cell");
+                node.style = `--x:${x + 1}; --y:${y + 1};`;
 
-                    node.classList.add("choosable");
-                }
+                let ship;
+                if (
+                    currData.shipsData.some((a) => {
+                        ship = a;
+                        return a[1] === x + "" && a[2] === y + "";
+                    })
+                ) insertShip(node, ship);
 
-                if (harbsPairs.some((a) => a[0] === j + "" && a[1] === i + "")) {
-                    node.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-anchor" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 9v12m-8 -8a8 8 0 0 0 16 0m1 0h-2m-14 0h-2" /><path d="M12 6m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /></svg>';
-
-                    node.classList.add("choosable");
-                }
+                let hrbr;
+                if (
+                    currData.harboursData.some((a) => {
+                        hrbr = a;
+                        return a[2] === x + "" && a[3] === y + "";
+                    })
+                ) insertHarbour(node, hrbr);
+                
 
                 map.appendChild(node);
             }
         }
     }
 };
+
+const insertShip = (node, ship) => {
+    node.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ship" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M2 20a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1" /><path d="M4 18l-1 -5h18l-2 4" /><path d="M5 13v-6h8l4 6" /><path d="M7 7v-4h-1" /></svg>';
+
+    node.classList.add("choosable");
+    node.classList.add("with-tooltip");
+
+    const tooltip = document.createElement("div");
+    tooltip.classList.add('tooltip');
+
+    const heldCargos = [];
+    currData.cargosData.forEach(c=>{
+        if(ship[4] == c[0]) {
+            heldCargos.push(c[0]);
+        }
+    })
+
+    const cargoTxt = `<fieldset><legend>Cargo</legend>${heldCargos.map(c=>`<p>${c}</p>`)}</fieldset>`;
+
+    tooltip.innerHTML = `<h3>${ship[0]}</h3>${cargoTxt}`;
+
+    node.appendChild(tooltip);
+}
+
+const insertHarbour = (node, hrbr) => {
+    node.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-anchor" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 9v12m-8 -8a8 8 0 0 0 16 0m1 0h-2m-14 0h-2" /><path d="M12 6m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /></svg>';
+
+    node.classList.add("choosable");
+    node.classList.add("with-tooltip");
+
+    const tooltip = document.createElement("div");
+    tooltip.classList.add('tooltip');
+
+    const heldShips = [];
+    currData.shipsData.forEach(s=>{
+        if(hrbr[0] == s[5]) {
+            heldShips.push(s[0]);
+        }
+    })
+
+    const heldCargos = [];
+    currData.cargosData.forEach(c=>{
+        if(hrbr[0] == c[1]) {
+            heldCargos.push(c[0]);
+        }
+    })
+
+    const heldCargosAwait = [];
+    currData.cargosData.forEach(c=>{
+        if(hrbr[0] == c[2]) {
+            heldCargosAwait.push(c[0]);
+        }
+    })
+
+    const shipsTxt = `<fieldset><legend>Ships</legend>${heldShips.map(s=>`<p>${s}</p>`)}</fieldset>`;
+    const cargoTxt = `<fieldset><legend>Origin Cargos</legend>${heldCargos.map(c=>`<p>${c}</p>`)}</fieldset>`;
+    const cargoAwaitedTxt = `<fieldset><legend>Awaited Cargos</legend>${heldCargosAwait.map(c=>`<p>${c}</p>`)}</fieldset>`;
+
+    tooltip.innerHTML = `<h3>${hrbr[1]}</h3>${shipsTxt}${cargoTxt}${cargoAwaitedTxt}`;
+
+    node.appendChild(tooltip);
+}
 
 getData();
 
